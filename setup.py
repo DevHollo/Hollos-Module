0e94aff191297e472599877ff6b98087748f91b3
Hollo <127456155+DevHollo@users.noreply.github.com>
2024-03-13 16:11:04 -0600

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+with open('README.md', 'r') as mdfile:
+    des = mdfile.read()
+
+setup(
+    name='Hollos-Module',
+    version='0.0.2',
+    scripts=['hollosmodule.py'],
+    author="Hollo",
+    author_email="hollo1234567890e@gmail.com",
+    long_description=des,
+    long_description_content_type="text/markdown",
+    install_requires=[
+        "sty==1.0.6",
+        "requests==2.31.0",
+        "getpass4==0.0.14.1",
+        "pillow==10.2.0"
+    ],
+    url="https://github.com/DevHollo/Hollos-Module"
+)
