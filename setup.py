e807e455263ac90d043e702131e3254e7548133e
Hollo <127456155+DevHollo@users.noreply.github.com>
2024-03-18 23:53:36 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as mdfile:
 
 setup(
     name='Hollos-Module',
-    version='0.0.2',
+    version='0.0.3',
     scripts=['hollosmodule.py'],
     author="Hollo",
     author_email="hollo1234567890e@gmail.com",
@@ -15,7 +15,8 @@ setup(
         "sty==1.0.6",
         "requests==2.31.0",
         "getpass4==0.0.14.1",
-        "pillow==10.2.0"
+        "pillow==10.2.0",
+        "qrcode==7.4.2"
     ],
     url="https://github.com/DevHollo/Hollos-Module"
 )
