bc5d5e7f43cf73a2a14154d3c5b3458951596e4f
Hollo <127456155+DevHollo@users.noreply.github.com>
2024-03-19 00:07:27 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as mdfile:
 
 setup(
     name='Hollos-Module',
-    version='0.0.4',
+    version='0.0.5',
     scripts=['hollosmodule.py'],
     author="Hollo",
     author_email="hollo1234567890e@gmail.com",
